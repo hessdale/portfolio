facc25f1b513314d630c8241505ce51e04cd893a
hessdale <hessdale96@gmail.com>
2024-01-24 21:22:28 -0800

    updated some info on the home page

--- src/components/HomeInfo.vue
+++ src/components/HomeInfo.vue
@@ -8,14 +8,15 @@
       <article id="about_me">
         <h2>About me</h2>
         <p>
-          I am currently in school for webdevelopment and enjoying it
-          thuroughly. Some thing about me are that I like to collect anything
-          music related. I enjoy movies and anime.
+          I graduated school for webdevelopment in July 2023 and have been enjoying exploring and learning on my own
+          while looking for work. Some things about me are that I like to collect anything
+          music related like shirts and CDs, I also have a large movie collection.
         </p>
       </article>
       <div id="profile_pic">
-        <!-- <img src="/images/SwhirlNobackgroundopaque.png" alt="black swirl" /> -->
-        <img src="/images/computer.png" alt="" />
+
+        <img src="/images/3d_computer.jpg" alt="pixel art computer" />
+
       </div>
       <article id="details">
         <div>
@@ -27,24 +28,18 @@
         </div>
         <div>
           <h4>Age</h4>
-          <p>27</p>
+          <p>28</p>
         </div>
         <div>
           <h4>Location</h4>
-          <p>Edmonton, Canada</p>
+          <p>Vancouver, Canada</p>
           <section>
-            <a href="https://github.com/hessdale"
-              ><img src="/images/github.svg" alt="github icon" width="30px"
-            /></a>
-            <a href="https://www.linkedin.com/in/hessdale96/"
-              ><img src="/images/linkedin.svg" alt="linkdin icon" width="30px"
-            /></a>
-            <a href="mailto:hessdale96@gmail.com"
-              ><img
-                src="/images/send-mail.svg"
-                alt="send mail icon"
-                width="33px"
-            /></a>
+            <a href="https://github.com/hessdale" target="_blank"><img src="/images/github.svg" alt="github icon"
+                width="30px" /></a>
+            <a href="https://www.linkedin.com/in/hessdale96/" target="_blank"><img src="/images/linkedin.svg"
+                alt="linkdin icon" width="30px" /></a>
+            <a href="mailto:hessdale96@gmail.com" target="_blank"><img src="/images/send-mail.svg" alt="send mail icon"
+                width="33px" /></a>
           </section>
         </div>
       </article>
@@ -53,9 +48,15 @@
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 <style scoped>
+main {
+  background-color: 240, 236, 236, 255;
+}
+
 #about_me {
   justify-self: center;
 }
@@ -71,16 +72,19 @@ p {
   letter-spacing: 1.2px;
   line-height: 25px;
 }
+
 main {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   margin: 40px;
 }
+
 #profile_pic {
   text-align: center;
   margin-top: 70px;
 }
-#profile_pic > img {
+
+#profile_pic>img {
   width: 60%;
 }
 
@@ -88,7 +92,7 @@ main {
   margin-top: 10px;
 }
 
-#details > div {
+#details>div {
   margin-top: 20px;
 }
 
@@ -99,20 +103,23 @@ main {
 #profile p {
   margin-top: 20px;
 }
+
 @media only screen and (min-width: 575px) {
   main {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(575px, 1fr));
   }
+
   #profile_pic {
     text-align: center;
   }
 
-  #profile_pic > img {
+  #profile_pic>img {
     width: 60%;
     max-width: 500px;
   }
 }
+
 @media only screen and (min-width: 1100px) {
   main {
     display: grid;
